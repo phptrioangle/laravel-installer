77cb76853951248a1cf862e9630f4f4592468f72
vignesh <pvignesh90@gmail.com>
2017-07-17 23:20:04 +0530

    revert conflicting changes - Vignesh

--- src/Helpers/InstalledFileManager.php
+++ src/Helpers/InstalledFileManager.php
@@ -12,17 +12,11 @@ class InstalledFileManager
      */
     public function create()
     {
-        $installedLogFile = storage_path('installed');
-        $dateStamp = date("Y/m/d h:i:sa");
-        if (!file_exists($installedLogFile))
-        {
-            $message = trans('installer_messages.installed.success_log_message') . $dateStamp . "\n";
-            file_put_contents($installedLogFile, $message);
-        } else {
-            $message = trans('installer_messages.updater.log.success_message') . $dateStamp;
-            file_put_contents($installedLogFile, $message.PHP_EOL , FILE_APPEND | LOCK_EX);
-        }
-        return $message;
+        file_put_contents(storage_path('installed'), '');
+
+        $result = file_get_contents(base_path('.env'));
+        $newLine = "APP_ENV=staging\nSESSION_DRIVER=database\n";
+        fwrite($result, $newLine);
     }
 
     /**
